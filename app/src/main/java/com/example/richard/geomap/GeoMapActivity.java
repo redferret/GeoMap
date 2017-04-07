db4c7679b1e7026969a8b0c23d813fba44ec4f92
Richard <rdesilvey@gmail.com>
2017-04-06 22:22:49 -0600

    First Commit

---
+++ app/src/main/java/com/example/richard/geomap/GeoMapActivity.java
@@ -0,0 +1,13 @@
+package com.example.richard.geomap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GeoMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_geo_map);
+    }
+}
