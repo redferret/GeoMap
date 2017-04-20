0c833fa3d203f973c888db0e2aaba3c713003710
Richard <rdesilvey@gmail.com>
2017-04-20 12:35:24 -0600

    Added two new fragments

--- app/src/main/java/com/example/richard/geomap/GeoMapActivity.java
+++ app/src/main/java/com/example/richard/geomap/GeoMapActivity.java
@@ -3,11 +3,24 @@ package com.example.richard.geomap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class GeoMapActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class GeoMapActivity extends AppCompatActivity  implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geo_map);
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
