021c8440448a96a72b9d96784b24abb941eb2bdc
Richard <rdesilvey@gmail.com>
2017-04-21 13:24:45 -0600

    Edits to Fragment and Location Class

--- app/src/main/java/Location.java
+++ app/src/main/java/Location.java
@@ -9,13 +9,39 @@ public class Location {
     //
     private String title;
     private LatLng position;
+    private double strike, dip;
 
-    public Location(double lat, double lng){
-        this(new LatLng(lat, lng));
-    }
 
     public Location(LatLng pos){
         this.position = pos;
     }
 
+    public Location(double lat, double lng){
+        this(new LatLng(lat, lng));
+    }
+
+    public void setStrikeDip(double strike, double dip){
+        this.strike = strike;
+        this.dip = dip;
+    }
+
+    public double getStrike() {
+        return strike;
+    }
+
+    public LatLng getPosition() {
+        return position;
+    }
+
+    public double getDip() {
+        return dip;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
