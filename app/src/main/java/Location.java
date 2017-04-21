2c783abab875e9d9442f27d6b6b9ee77afc2c792
Richard <rdesilvey@gmail.com>
2017-04-21 12:40:19 -0600

    Added Fragment

--- app/src/main/java/Location.java
+++ app/src/main/java/Location.java
@@ -1,14 +1,14 @@
 import com.google.android.gms.maps.model.LatLng;
 
 /**
- * Created by Richard on 4/20/2017.
+ *
  */
-
 public class Location {
 
+    // Images
+    //
     private String title;
     private LatLng position;
-
 
     public Location(double lat, double lng){
         this(new LatLng(lat, lng));
